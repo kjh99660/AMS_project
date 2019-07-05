fccfbb0cbf4d3a57d8f50245bde5ac2ecf10bb9c
kjh99660 <kdh4021200@naver.com>
2019-07-06 02:27:13 +0900

    수정

--- lobster/lobster/Assets/Spawner.cs
+++ lobster/lobster/Assets/Spawner.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject CreateBear = null;
-    public float Spown_term = 3.0f;//스폰되는 간격
-    public int Bear_number = 3;//몹 수
+    
 
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-        Spown_term -= Time.deltaTime;  // 리스폰 시간을 깍음. 
-        if (Spown_term < 0 && Bear_number>0)                  // 리스폰 시간이 0이 되었는지 검사 
+        Bear.Spown_term -= Time.deltaTime;  // 리스폰 시간을 깍음. 
+        if (Bear.Spown_term < 0 && Bear.Bear_number > 0)                  // 리스폰 시간이 0이 되었는지 검사 
         {
-            Instantiate(CreateBear, new Vector3(-10, 0.1f, 1), Quaternion.identity); // 생성
-            
-            Spown_term = 3.0f;// 리스폰시간 초기화
-            Bear_number--;
+            if (Bear.stage_level == 1)
+            {
+                Instantiate(CreateBear, new Vector3(-10, 0.1f, 1), Quaternion.identity); // 생성
+
+                Bear.Spown_term = 3.0f;// 리스폰시간 초기화
+                Bear.Bear_number--;
+            }
+            if(Bear.stage_level == 2)
+            {
+                Instantiate(CreateBear, new Vector3(-9, -13, 1), Quaternion.identity); // 생성
+
+                Bear.Spown_term = 3.0f;// 리스폰시간 초기화
+                Bear.Bear_number--;
+            }
 
         }
+        
     }
 }
