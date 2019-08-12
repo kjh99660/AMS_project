a9769bad9758c3a5752b9d4e7d620f535cf6a27d
kjh99660 <kdh4021200@naver.com>
2019-08-12 20:56:28 +0900

    1.0.3.1

---
+++ lobster/Assets/Scripts/sound/SManager2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SManager2 : bgmSlider
+{
+
+    public static SManager2 instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+
+            Destroy(gameObject);
+
+
+    }
+
+
+}
---
+++ lobster/Assets/Scripts/sound/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static SoundManager instance = null;
+
+    //초기화
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            
+            Destroy(gameObject);
+
+    
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+}
